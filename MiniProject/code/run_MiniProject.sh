305fec9eec58aaf9b67a9762cddf1b50146659ee
Cherie Yu <cy221@ic.ac.uk>
2021-12-03 11:20:43 +0000

    miniproject

--- MiniProject/code/run_MiniProject.sh
+++ MiniProject/code/run_MiniProject.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
 python3 data_wrangle.py
+echo "Data is being wrangled"
 python3 sample.py
+echo "Starting values are sampled"
+echo "All plots can be found in graph directory"
 python3 AIC.py
+echo "AIC values are being calculated"
 Rscript example_plot.R
 Rscript AIC_summary.R
-source bash.sh Report 
+source bash.sh Report
+echo "Compiling LaTex into Report.pdf"
