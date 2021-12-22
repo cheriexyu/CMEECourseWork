78e5dd0708cd3fa217ae4ac74254bcf118c4ae4e
Cherie Yu <cy221@ic.ac.uk>
2021-12-21 20:41:34 -0800

    project proposal

---
+++ Proposalinside/bash_copy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+pdflatex project_proposal.tex #generates two files .log and .aux (and an incomplete .pdf)
+bibtex project_proposal #reads .aux and produce two files .bbl and .blg, bibtex put citation into aux file
+pdflatex project_proposal.tex #run updates on .log and .aux (writes in text on .aux)
+pdflatex project_proposal.tex #updates .log and .aux and produce final .pdf
+
+## Cleanup
+rm *.aux
+rm *.log
+rm *.bbl
+rm *.blg
